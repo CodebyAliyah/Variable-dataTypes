7c137e86e763de9e43d36524e4decf4f9a1ed842
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-08-31 05:06:01 -0700

    Code Done

---
+++ Variable_&_DataTypes.js
@@ -0,0 +1,50 @@
+
+/**** Activity 1 : Variable Declaration ****/
+
+var number1 = 33 ;
+console.log(number1)
+
+
+let number2 = "CodebyAliYah"
+console.log(number2)
+
+/**** Activity 2 : Constant Declaration ****/
+
+const bolEan1 = Boolean(NaN);
+console.log(bolEan1)
+
+let bolEan2 = Boolean(undefined)
+console.log(bolEan2)
+
+/**** Activity 3 : Data Types ****/
+
+const myNum1 = 100 ;
+console.log(typeof myNum1)
+
+const myNum2 = "CodebyAliYah"
+console.log(typeof myNum2)
+
+
+const myNum3 = false ;
+console.log(typeof myNum3)
+
+
+const myNum4 = [1 , 2 , 3 , 4 [3 , 5, 8 ,8]]
+console.log(typeof myNum4)
+
+const myNum5 = {}
+console.log(typeof myNum5)
+
+/**** Activity 4 : Reassigning Variables ****/
+
+let val1 = "Ali"
+let val2 = "Yahya"
+console.log(`value one is : ${val1} and value 2 is ${val2}`)
+
+/**** Activity 5 : Understanding const ****/
+
+const value1 = 22 ;
+const value2 = "Ali Yahya" ;
+
+console.log(value1 + " " + value2)
+
